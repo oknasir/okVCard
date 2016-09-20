59ff5ead7d08a9e0b9cbdc737c19b8652aec58c6
Nasir Mehmood <oknasir@gmail.com>
2016-09-20 20:49:07 +0500

    latest version for platform set

---
+++ readme.md
@@ -0,0 +1,9 @@
+# okVCard
+
+Basic template for oknasir Resume. It has basic admin panel for update frontend pages.
+
+## Extensions Used
+ - [Laravel **5**](http://laravel.com/docs)
+ - [Bootstrap **3**](http://laravel.com/docs)
+ - [Vuejs **1**](http://vuejs.org/guide)
+ - [jQuery **3**](https://api.jquery.com)
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+
+    </head>
+    <body>
+
+        <section class="container">
+
+            <h1>Laravel</h1>
+
+            <example></example>
+
+        </section>
+
+        <script src="{{ elixir('js/app.js') }}"></script>
+
+    </body>
+</html>
